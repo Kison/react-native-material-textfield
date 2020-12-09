3e704566cc74806f821f935391127a13478066f4
Denis Kison <denkison@gmail.com>
2020-12-09 16:36:34 +0200

    React Native 0.62 Fix

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -215,6 +215,10 @@ export default class TextField extends PureComponent {
     if (labelState ^ prevLabelState) {
       this.startLabelAnimation();
     }
+
+    if (prevProps.value !== this.props.value) {
+      this.setValue(this.props.value);
+    }
   }
 
   startFocusAnimation() {
@@ -223,6 +227,7 @@ export default class TextField extends PureComponent {
 
     let options = {
       toValue: this.focusState(),
+      useNativeDriver: false,
       duration,
     };
 
@@ -235,7 +240,7 @@ export default class TextField extends PureComponent {
 
     let options = {
       toValue: this.labelState(),
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration,
     };
 
